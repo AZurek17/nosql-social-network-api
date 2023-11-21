40fcac8d04d3d07f0575cd667b8bde7fac503b28
Andy <84168944+AZurek17@users.noreply.github.com>
2023-11-20 19:19:34 -0700

    added user routes Get, post, put , delete

---
+++ routes/api/Users-routes.js
@@ -0,0 +1,63 @@
+const app = require('express').Router();
+
+// Get all users route 
+
+app.get('/users', async (req, res) => {
+    try {
+      // Using model in route to find all documents that are instances of that model
+      const result = await User.find({});
+      res.status(200).json(result);
+    } catch (err) {
+      console.log('Uh Oh, something went wrong');
+      res.status(500).json({ message: 'something went wrong' });
+    }
+  });
+
+// Get one users route 
+  app.get('/users/{_id}', async (req, res) => {
+    try {
+      const result = await User.findById({ _id: req.params._id })
+        .populate('thoughts')
+        .populate('friends');
+      res.status(200).json(result);
+    } catch (err) {
+      console.log('Uh Oh, something went wrong');
+      res.status(500).json({ message: 'something went wrong' });
+    }
+  });
+
+// create new user route
+app.post('/users', (req, res) => {
+    const newUser = new User({ username: req.params.username, email: req.params.email, thoughts: req.params.thoughts, friends: req.params.friends });
+    newUser.save();
+    if (newUser) {
+      res.status(200).json(newUser);
+    } else {
+      console.log('Uh Oh, something went wrong');
+      res.status(500).json({ message: 'something went wrong' });
+    }
+  });
+
+// Post update user route
+app.update('/users/{_id}', async (req, res) => {
+    try {
+      const result = await Users.findOneAndUpdate({ _id: req.params._id }, { $set: req.body });
+      res.status(200).json(result);
+      console.log(`Deleted: ${result}`);
+    } catch (err) {
+      console.log('Uh Oh, something went wrong');
+      res.status(500).json({ message: 'something went wrong' });
+    }
+  });
+
+// Post delete user route
+app.delete('/users/{_id}', async (req, res) => {
+    try {
+      const result = await Users.findOneAndDelete({ _id: req.params._id });
+      res.status(200).json(result);
+      console.log(`Deleted: ${result}`);
+    } catch (err) {
+      console.log('Uh Oh, something went wrong');
+      res.status(500).json({ message: 'something went wrong' });
+    }
+  });
