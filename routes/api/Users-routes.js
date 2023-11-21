a54026b1e190db01045af2f766ffc9312e3b3484
Andy <84168944+AZurek17@users.noreply.github.com>
2023-11-20 19:25:11 -0700

    added friends post + put routes to Users-routes

--- routes/api/Users-routes.js
+++ routes/api/Users-routes.js
@@ -61,3 +61,26 @@ app.delete('/users/{_id}', async (req, res) => {
       res.status(500).json({ message: 'something went wrong' });
     }
   });
+
+  // create new Friend route
+app.post('/users/:userID/friends/:friendId', (req, res) => {
+    const newUser = new Friends({ username: req.params.username, email: req.params.email, thoughts: req.params.thoughts, friends: req.params.friends });
+    newUser.save();
+    if (newUser) {
+      res.status(200).json(newUser);
+    } else {
+      console.log('Uh Oh, something went wrong');
+      res.status(500).json({ message: 'something went wrong' });
+    }
+  });
+//  delete friend route
+app.delete('/users/:userID/friends/:friendId', async (req, res) => {
+    try {
+      const result = await Friends.findOneAndDelete({ _id: req.params._id });
+      res.status(200).json(result);
+      console.log(`Deleted: ${result}`);
+    } catch (err) {
+      console.log('Uh Oh, something went wrong');
+      res.status(500).json({ message: 'something went wrong' });
+    }
+  });
